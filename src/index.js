b3e4671015e74f7a1fdda9d166acf2ba1c1bee9e
nsensical <iss.doli@gmail.com>
2024-10-20 19:40:14 -0400

    updated js

--- src/index.js
+++ src/index.js
@@ -14,9 +14,13 @@ function generateRecipe(event) {
   let instructionsInput = document.querySelector("#instructions");
   let apiKey = "abdtf489166e9o460ad2e43465ab3d66";
   let context =
-    "You are a knowledgeable assistant with vast experience in international cuisine. Generate short and concise Venezuelan recipes making sure to follow the instructions, using basic HTML and separating each line with a <br />. Do not display the word html nor add backticks";
+    "You are a knowledgeable assistant with vast experience in international cuisine. Generate short and concise Venezuelan recipes making sure to follow the instructions, using basic HTML and separating each line with a <br />.";
   let prompt = `Instructions: Generate a Venezuelan recipe about ${instructionsInput.value}.`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating">⏳ Wait a moment... Searching for a good recipe containing ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayRecipe);
 }
