e7b402ff650fc8796d7319609de6722eccd7560b
nsensical <iss.doli@gmail.com>
2024-10-20 16:45:59 -0400

    added javascript
    basic javascript and typewriter script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 /** @format */
+
+function generateRecipe(event) {
+  event.preventDefault();
+  new Typewriter("#recipe", {
+    strings: "Quesillo",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
